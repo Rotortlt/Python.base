cf24600e28205c4b4d948f61304df1a10e4880f9
Viktor Sotnikov <sotnikov_victor@mail.ru>
2019-02-25 20:23:51 +0300

    add task#1 normal level

---
+++ Lesson1/task1 norm.py
@@ -0,0 +1,20 @@
+﻿import random
+randomNumber = int(random.random() * 1000000)
+maxNumber = 0
+print(randomNumber)
+
+while randomNumber > 0:
+    randomIntegerNumber = int(randomNumber // 10)
+    randomNumber = round((randomNumber / 10), 1)
+    randomNumberI = int(round((randomNumber * 10),1)) - (randomIntegerNumber * 10)
+    randomNumber = randomIntegerNumber
+    if randomNumberI > maxNumber:
+        maxNumber = randomNumberI
+
+print(maxNumber)
+
+
+
+
+
+
