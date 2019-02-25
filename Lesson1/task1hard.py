4e1491570a89298afa06dd0162af7ae74e4aaa75
Viktor Sotnikov <sotnikov_victor@mail.ru>
2019-02-25 22:14:39 +0300

    finish task1 hard level

--- Lesson1/task1hard.py
+++ Lesson1/task1hard.py
@@ -1,4 +1,18 @@
-﻿number = int(input("Введите число: ")
+﻿number = int(input("Введите число: "))
+count = 0
+i = 0
+while count < 3:
+    i = i + 1
+    if number % i == 0:
+        count = count + 1
+    if count > 2:
+        print("Вы ввели число ", number, " оно сложное")
+        break
+    if i == number:
+        print("Вы ввели число ", number, " оно простое")
+        break
+
+
 
 
 
