cfa76106006b4c5eaaaaf2b0bc62cfe7da86347a
Viktor Sotnikov <sotnikov_victor@mail.ru>
2019-02-25 19:50:06 +0300

    finish 1 task easy level

--- Lesson1/less 1.py
+++ Lesson1/less 1.py
@@ -1,9 +1,13 @@
 ﻿import random
 randomNumber = int(random.random() * 1000000)
-while randomNumber > 0
-    randomNumber = randomNumber / 10
-    randomIntegerNumber = randomNumber // 10
-    randomNumberI = (randomNumber - randomIntegerNumber) * 10
+print(randomNumber)
+
+while randomNumber > 0:
+    randomIntegerNumber = int(randomNumber // 10)
+    randomNumber = round((randomNumber / 10), 1)
+    randomNumberI = int(round((randomNumber * 10),1)) - (randomIntegerNumber * 10)
+    randomNumber = randomIntegerNumber
+
     print(randomNumberI)
 
 
