6aa3d4c9c8867c6464834dac3734e1a25b8eab64
Viktor Sotnikov <sotnikov_victor@mail.ru>
2019-03-03 15:50:21 +0300

    finish task 1 easy level

--- Lesson3/task1_easy.py
+++ Lesson3/task1_easy.py
@@ -12,22 +12,8 @@ count_mark = int(input("Введите до какого количества з
 
 print(ran_number)
 
-
 def round_number(number, count_mark):
-
-    integer_num = int(number * 10 ** count_mark )
-    fraction_residue = ((number * 10 ** count_mark ) - integer_num)
-
-    if fraction_residue > 5:
-        fraction_residue = int(fraction_residue + 1)
-
-    else:
-        fraction_residue = int(fraction_residue)
-
-
-    num = (integer_num + fraction_residue) / 10 ** count_mark
-
+    num = round(number, count_mark)
     return  num
-
 
 print(f"Округленная число: {round_number(ran_number, count_mark)}")
