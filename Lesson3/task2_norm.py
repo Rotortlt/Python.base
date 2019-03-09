29205d459103b5d1897731affe8746502a0182c1
Viktor Sotnikov <sotnikov_victor@mail.ru>
2019-03-09 13:31:01 +0300

    start and finish task 2 norm level

---
+++ Lesson3/task2_norm.py
@@ -0,0 +1,20 @@
+# Задача-2:
+# Напишите функцию, сортирующую принимаемый список по возрастанию.
+# Для сортировки используйте любой алгоритм (например пузырьковый).
+# Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
+
+list_number = [100,232,13,46,25,16,7]
+
+
+def sort_func(numbers):
+
+    for i in range (len(numbers) - 1):
+        for j in range (len(numbers) - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    return numbers
+
+
+
+
+print(sort_func(list_number))
